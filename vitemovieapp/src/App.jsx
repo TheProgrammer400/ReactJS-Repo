63b5c0b7a30d5895f22bd1fcf8479995e56b35db
The Programmer <shrey.b.tank@gmail.com>
2025-03-31 17:07:13 +0530

    routers & navbar added

--- vitemovieapp/src/App.jsx
+++ vitemovieapp/src/App.jsx
@@ -1,11 +1,21 @@
+import "./App.css"
 import MovieCard from "./components/MovieCard.jsx"
 import Home from "./pages/Home.jsx"
+import {Route, Routes} from "react-router-dom"
+import Favorites from "./pages/Favorites.jsx"
+import NavBar from "./components/NavBar.jsx"
 
 function App() {
     return (
-        <>
-            <Home />
-        </>
+        <div>
+            <NavBar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </main>
+        </div>
     )
 }
 
---
+++ vitemovieapp/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites(){
+    return (
+        <div className="favorites-emtpy">
+            <h2>No favorites movie yet</h2>
+            <p>Start adding movies to your favorites and they will appear here</p>
+        </div>
+    )
+}
+
+export default Favorites
